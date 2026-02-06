60af47dc18b38d4eff07287c64047ee6fc124edb
NagavardhanMothukuri <121569913+NagavardhanMothukuri@users.noreply.github.com>
2026-02-06 18:35:56 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import UserDashboard from '../components/userDashboard'
+import UserDashboard from '../components/UserDashboard'
 import OwnerDashboard from '../components/OwnerDashboard'
 import DeliveryBoy from '../components/DeliveryBoy'
 import { useTheme } from '../context/ThemeContext'
 
 function Home() {
-    const {userData}=useSelector(state=>state.user)
+    const { userData } = useSelector(state => state.user)
     const { currentMode } = useTheme()
     
-  return (
-    <div className={`w-[100vw] min-h-[100vh] pt-[100px] flex flex-col items-center transition-all duration-500 ${currentMode.bg}`}>
-      {userData.role=="user" && <UserDashboard/>}
-      {userData.role=="owner" && <OwnerDashboard/>}
-      {userData.role=="deliveryBoy" && <DeliveryBoy/>}
-    </div>
-  )
+    return (
+        <div className={`w-[100vw] min-h-[100vh] pt-[100px] flex flex-col items-center transition-all duration-500 ${currentMode.bg}`}>
+            {userData.role === "user" && <UserDashboard />}
+            {userData.role === "owner" && <OwnerDashboard />}
+            {userData.role === "deliveryBoy" && <DeliveryBoy />}
+        </div>
+    )
 }
 
 export default Home
